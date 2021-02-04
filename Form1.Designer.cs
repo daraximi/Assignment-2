0ab4dc8406526e4fcc5974e97ec3fe495a17c73c
Oloyede Oluwadarasimi <75390273+daraximi@users.noreply.github.com>
2021-02-04 09:38:48 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLesdDet = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(847, 589);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Full Details";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 650);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnLesdDet
+            // 
+            this.btnLesdDet.Location = new System.Drawing.Point(847, 550);
+            this.btnLesdDet.Name = "btnLesdDet";
+            this.btnLesdDet.Size = new System.Drawing.Size(75, 23);
+            this.btnLesdDet.TabIndex = 3;
+            this.btnLesdDet.Text = "MAP";
+            this.btnLesdDet.UseVisualStyleBackColor = true;
+            this.btnLesdDet.Click += new System.EventHandler(this.btnLesdDet_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 100;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 15;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ABIA",
+            "ADAMAWA",
+            "AKWA IBOM",
+            "ANAMBRA",
+            "BAUCHI",
+            "BAYELSA",
+            "BENUE",
+            "BORNO",
+            "CROSS RIVER",
+            "DELTA",
+            "EBONYI",
+            "EDO",
+            "EKITI",
+            "ENUGU",
+            "FCT",
+            "GOMBE",
+            "IMO",
+            "JIGAWA",
+            "KADUNA",
+            "KANO",
+            "KATSINA",
+            "KEBBI",
+            "KOGI",
+            "KWARA",
+            "LAGOS",
+            "NASSARAWA",
+            "NIGER",
+            "OGUN",
+            "ONDO",
+            "OSUN",
+            "OYO",
+            "PLATEAU",
+            "RIVERS",
+            "SOKOTO",
+            "TARABA",
+            "YOBE",
+            "ZAMFARA"});
+            this.comboBox1.Location = new System.Drawing.Point(810, 54);
+            this.comboBox1.MaxDropDownItems = 5;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Select a state";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(809, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IGR :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(810, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "In 2019";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(847, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "e";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnLesdDet);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "IGR 2019 BY dARA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLesdDet;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public const string A = "ABIA";
+        public const string B = "ADAMAWA";
+        public const string C = "AKWA IBOM";
+        public const string D = "ANAMBRA";
+        public const string E = "BAUCHI";
+        public const string F = "BAYELSA";
+        public const string G = "BENUE";
+        public const string H = "BORNO";
+        public const string I = "CROSS RIVER";
+        public const string J = "DELTA";
+        public const string K = "EBONYI";
+        public const string L = "EDO";
+        public const string M = "EKITI";
+        public const string N = "ENUGU";
+        public const string O = "FCT";
+        public const string P = "GOMBE";
+        public const string Q = "IMO";
+        public const string R = "JIGAWA";
+        public const string S = "KADUNA";
+        public const string T = "KANO";
+        public const string U = "KATSINA";
+        public const string V = "KEBBI";
+        public const string W = "KOGI";
+        public const string X = "KWARA";
+        public const string Y = "LAGOS";
+        public const string Z = "NASSARAWA";
+        public const string AA = "NIGER";
+        public const string AB = "OGUN";
+        public const string AC = "ONDO";
+        public const string AD = "OSUN";
+        public const string AE = "OYO";
+        public const string AF = "PLATEAU";
+        public const string AG = "RIVERS";
+        public const string AH = "SOKOTO";
+        public const string AI = "TARABA";
+        public const string AJ = "YOBE";
+        public const string AK = "ZAMFARA";
+
+
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+
+        }
+
+        public void label1_Click (object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnClickThis_Click (object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = WindowsFormsApp1.Properties.Resources.igr;
+
+        }
+
+        public void pictureBox1_Click (object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnLesdDet_Click (object sender, EventArgs e)
+        {
+            pictureBox1.Image = WindowsFormsApp1.Properties.Resources.nigeria_administrative_map;
+        }
+
+        public void comboBox1_SelectedIndexChanged (object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == A)
+            {
+                label2.Text = "IGR : 14,769,307,658.56";
+            }
+            if (comboBox1.SelectedItem == B)
+            {
+                label2.Text = "IGR : 9,704,660,185.44";
+            }
+            if (comboBox1.SelectedItem == C)
+            {
+                label2.Text = "IGR : 32,291,014,771.52";
+            }
+            if (comboBox1.SelectedItem == D )
+            {
+                label2.Text = "IGR : 26,369,195,864.89";
+            }
+            if (comboBox1.SelectedItem ==  E)
+            {
+                label2.Text = "IGR:  11,696,955,884.75";
+            }
+            if (comboBox1.SelectedItem == F)
+            {
+                label2.Text = "IGR:  16,342,762,531.99";
+            }
+            if (comboBox1.SelectedItem == G)
+            {
+                label2.Text = "IGR:  17,850,480,389.57";
+            }
+            if (comboBox1.SelectedItem == H)
+            {
+                label2.Text = "IGR:  8,175,248,326.42";
+            }
+            if (comboBox1.SelectedItem == I)
+            {
+                label2.Text = "IGR:  22,597,063,882.55";
+            }
+            if (comboBox1.SelectedItem == J)
+            {
+                label2.Text = "IGR:  64,678,796,991.57";
+            }
+            if (comboBox1.SelectedItem == K)
+            {
+                label2.Text = "IGR:  7,455,294,676.59";
+            }
+            if (comboBox1.SelectedItem == L)
+            {
+                label2.Text = "IGR:  29,478,406,024.31";
+            }
+            if (comboBox1.SelectedItem == M)
+            {
+                label2.Text = "IGR:  8,546,875,648.24";
+            }
+            if (comboBox1.SelectedItem == N)
+            {
+                label2.Text = "IGR:  31,069,466,913.00";
+            }
+            if (comboBox1.SelectedItem == O)
+            {
+                label2.Text = "IGR:  74,564,180,835.31";
+            }
+            if (comboBox1.SelectedItem == P)
+            {
+                label2.Text = "IGR:  6,803,064,814.10";
+            }
+            if (comboBox1.SelectedItem == Q)
+            {
+                label2.Text = "IGR:  16,095,299,620.59";
+            }
+            if (comboBox1.SelectedItem == R)
+            {
+                label2.Text = "IGR:  12,926,658,146.29";
+            }
+            if (comboBox1.SelectedItem == S)
+            {
+                label2.Text = "IGR:  44,956,576,583.38";
+            }
+            if (comboBox1.SelectedItem == T)
+            {
+                label2.Text = "IGR:  40,593,701,332.48";
+            }
+            if (comboBox1.SelectedItem == U)
+            {
+                label2.Text = "IGR:  8,496,742,119.00";
+            }
+            if (comboBox1.SelectedItem == V)
+            {
+                label2.Text = "IGR:  7,367,334,837.13";
+            }
+            if (comboBox1.SelectedItem == W)
+            {
+                label2.Text = "IGR:  16,389,026,388.86";
+            }
+            if (comboBox1.SelectedItem == X)
+            {
+                label2.Text = "IGR:  30,646,731,408.92";
+            }
+            if (comboBox1.SelectedItem == Y)
+            {
+                label2.Text = "IGR:  398,732,246,493.38";
+            }
+            if (comboBox1.SelectedItem == Z)
+            {
+                label2.Text = "IGR:  10,858,822,422.98";
+            }
+            if (comboBox1.SelectedItem == AA)
+            {
+                label2.Text = "IGR:  12,765,034,972.30";
+            }
+            if (comboBox1.SelectedItem == AB)
+            {
+                label2.Text = "IGR:  70,922,590,495.89";
+            }
+            if (comboBox1.SelectedItem == AC)
+            {
+                label2.Text = "IGR:  30,135,881,918.26";
+            }
+            if (comboBox1.SelectedItem == AD)
+            {
+                label2.Text = "IGR:  17,922,394,523.43";
+            }
+            if (comboBox1.SelectedItem == AE)
+            {
+                label2.Text = "IGR:  26,746,460,235.93";
+            }
+            if (comboBox1.SelectedItem == AF)
+            {
+                label2.Text = "IGR:  16,480,111,593.83";
+            }
+            if (comboBox1.SelectedItem == AG)
+            {
+                label2.Text = "IGR:  140,398,744,302.70";
+            }
+            if (comboBox1.SelectedItem == AH)
+            {
+                label2.Text = "IGR:  19,005,093,541.11";
+            }
+            if (comboBox1.SelectedItem == AI)
+            {
+                label2.Text = "IGR:  6,533,106,447.27";
+            }
+            if (comboBox1.SelectedItem == AJ)
+            {
+                label2.Text = "IGR:  8,444,634,099.09";
+            }
+            if (comboBox1.SelectedItem == AK)
+            {
+                label2.Text = "IGR:  15,416,043,399.76";
+            }
+        }
+    }
+}
